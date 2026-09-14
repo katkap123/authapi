b506d8e0093d53cee335330aa18b0153b6a31242
Swathi Vutlapalli <swathivutlapalli@Swathis-MacBook-Pro.local>
2026-09-14 00:26:30 -0500

    fix cors issue

--- src/main/java/com/katta/login/config/SecurityConfig.java
+++ src/main/java/com/katta/login/config/SecurityConfig.java
@@ -1,21 +1,19 @@
 package com.katta.login.config;
 
-import com.katta.login.security.JwtAuthenticationFilter;
-import com.katta.login.security.RestAccessDeniedHandler;
-import com.katta.login.security.RestAuthenticationEntryPoint;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.katta.login.security.JwtAuthenticationFilter;
+import com.katta.login.security.RestAccessDeniedHandler;
+import com.katta.login.security.RestAuthenticationEntryPoint;
 
 @Configuration
 public class SecurityConfig {
@@ -74,7 +72,8 @@ public class SecurityConfig {
             .addFilterBefore(
                 jwtAuthenticationFilter,
                 UsernamePasswordAuthenticationFilter.class
-            );
+            )
+            .cors(Customizer.withDefaults());
 
         return http.build();
     }
